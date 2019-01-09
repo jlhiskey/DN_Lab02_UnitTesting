c43715040a26a2a2c7ce33a27948769f66aa34f4
Jason Hiskey <jasonhi@crazyredhead.com>
2019-01-08 20:06:06 -0800

    Sketched out initial framwork of logic.

---
+++ DN_Lab02_UnitTesting/ATM_Tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+using System;
+using Xunit;
+using DN_Lab02_UnitTesting;
+
+namespace ATM_Tests
+{
+    public class UnitTest1
+    {
+        //Test Balance
+        [Fact]
+        public void TestingBalance()
+        {
+
+        }
+        //Test Withdraw
+        [Fact]
+        public void TestingWithdraw()
+        {
+
+        }
+        //Test Deposit
+        [Fact]
+        public void TestingDeposit()
+        {
+
+        }
+        //Maybe Test Transaction History
+    }
+}
---
+++ DN_Lab02_UnitTesting/DN_Lab02_UnitTesting/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DN_Lab02_UnitTesting
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        //TODO: User Interface
+        //TODO: Check Balance
+        //TODO: Withdraw 
+        //TODO: Deposit
+        //TODO: Transaction History --Stretch
+    }
+}
