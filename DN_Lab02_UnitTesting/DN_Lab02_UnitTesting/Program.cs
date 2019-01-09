19120802f03a6678d1be7891846a8acc959f9099
Jason Hiskey <jasonhi@crazyredhead.com>
2019-01-08 20:34:08 -0800

    Completed UserInterface method

--- DN_Lab02_UnitTesting/DN_Lab02_UnitTesting/Program.cs
+++ DN_Lab02_UnitTesting/DN_Lab02_UnitTesting/Program.cs
@@ -6,9 +6,43 @@ namespace DN_Lab02_UnitTesting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Jason's ATM");
+            UserInterface();
         }
         //TODO: User Interface
+        static void UserInterface()
+        {
+            Console.WriteLine("Select 0 to Exit\nSelect 1 for Balance\nSelect 2 for Withdrawal\nSelect 3 for Deposit\nSelect 4 for Transaction History");
+            byte userInput = 0;
+            try
+            {
+                userInput = byte.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Incorrect Format");
+                UserInterface();
+            }
+            switch (userInput)
+            {
+                case 0:
+                    Environment.Exit(1);
+                    break;
+                case 1:
+                    Balance();
+                    break;
+                case 2:
+                    Withdrawal();
+                    break;
+                case 3:
+                    Deposit();
+                    break;
+                case 4:
+                    TransactionHistory();
+                    break;
+                default:
+            }
+        }
         //TODO: Check Balance
         //TODO: Withdraw 
         //TODO: Deposit
